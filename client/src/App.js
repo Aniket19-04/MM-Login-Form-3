da9f851d1f812014312f4768a9ee0e10ad0fec03
Aniket19-04 <aniketquattrolover0619@gmail.com>
2022-08-13 11:41:30 +0530

    first commit

---
+++ client/src/App.js
@@ -0,0 +1,19 @@
+import './App.css';
+import InpStud from './components/InpStud';
+
+function App() {
+  return (
+    <>
+      <div className="text-box">
+        <header>
+          <h1>Murder Mystery</h1>
+          <h2>Registration Page: </h2>
+        </header>
+      </div>
+      <InpStud></InpStud>
+
+    </>
+  );
+}
+
+export default App;
